d3d08928261daf54e14df49f1ee1983552db4eab
Ali Zahid Alavi <aalavi2401@conestogac.on.ca>
2026-04-10 18:31:49 -0400

    Added predict() function to classifier.c

--- classifier.c
+++ classifier.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 void train();
-
+void predict();
 
 int main(){
 
     train();
-
+    predict();
     return 0;
 }
 
 void train(){
     printf("train");
+}
+
+void predict(){
+    printf("predict");
 }
